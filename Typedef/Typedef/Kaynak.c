cd9b4d5c197ef4cf6ef4b410ee1ee923a3da5539
ErayMersin <116799603+ErayMersin@users.noreply.github.com>
2022-11-20 11:00:54 +0300

    typedef

---
+++ Typedef/Typedef/Kaynak.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef int d;
+
+int main() {
+
+    d d1;
+    d d2;
+    d d3;
+    d2 = 20;
+    d3 = 50;
+    d1 = d3 + d2;
+    printf("d1= %d", d1);
+
+    return 0;
+}
